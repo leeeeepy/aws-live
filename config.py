c3411d6809df0fe01262ec28519653927b0ed5a6
leeeeepy <114242735+leeeeepy@users.noreply.github.com>
2023-12-19 11:44:18 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cpkus29yvixy.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leepeiyee-employee"
+customregion = "us-east-1"
+
